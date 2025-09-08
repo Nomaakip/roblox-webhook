d9faa799242c0440b6533a54252f9f6a6fb466ea
hacks.guide <128547338+Nomaakip@users.noreply.github.com>
2025-09-08 13:38:33 +0200

    Create script.lua

---
+++ src/script.lua
@@ -0,0 +1,18 @@
+	local HttpService = game:GetService("HttpService")
+	local url = "your_discord_webhook_url"
+
+	game.Players.PlayerAdded:Connect(function(player)
+		player.Chatted:Connect(function(msg)
+		    local pfp_url = "https://api.newstargeted.com/roblox/users/v1/avatar-headshot?userid="..player.UserId.."&size=150x150&format=Png&isCircular=false"
+			print(pfp_url)
+
+			local data = {
+				username = player.Name,
+				content = msg,
+				avatar_url = pfp_url
+			}
+			
+			local response = HttpService:PostAsync(url, HttpService:JSONEncode(data))
+			print(response)
+		end)
+	end)
